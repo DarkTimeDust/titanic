afd742a3c8f91d2aa469614d181130f649dad5f2
DarkTimeDust <107348450+DarkTimeDust@users.noreply.github.com>
2023-10-06 17:53:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 df = pd.read_csv('titanic.csv')
 
@@ -6,7 +10,9 @@ df.drop(['PassengerId','Name', "Ticket", 'Cabin'],
     axis=1, inplace = True
     )
 
-df['Embarked'].fillna('s', inplace=True)
+df['Embarked'].fillna('S', inplace=True)
+
+#роки
 
 age_1 = df[df['Pclass']==1]['Age'].median()
 age_2 = df[df['Pclass']==2]['Age'].median()
@@ -24,10 +30,12 @@ def set_age(row):
 
 df['Age'] = df.apply(set_age, axis=1)
 
+#стать
+
 def set_sex(sex):
     if sex == 'male':
         return 1
-    elif sex == 'famale':
+    elif sex == 'female':
         return 0 
     
 df['Sex']= df['Sex'].apply(set_sex)
@@ -38,3 +46,26 @@ df.drop('Embarked',axis=1, inplace=True)
 
 df.info()
 
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(X_train,y_train)
+
+y_pred = classifier.predict(X_test)
+
+print(y_pred)
+
+for p,t in zip(y_pred,y_test):
+    print(f'p={p}; t={t}')
+
+precent = accuracy_score(y_test, y_pred)*100
+print(f'aaaa{precent}%')
+
+print(confusion_matrix(y_test,y_pred))
